d97b984da112b71c504d276ec9b8e762fe736a38
Chelaru Adrian <che.adrian@yahoo.com>
2023-11-07 10:44:31 +0000

    Modify module description

--- custom_components/thunderboard/thunderboard_ble/__init__.py
+++ custom_components/thunderboard/thunderboard_ble/__init__.py
@@ -1,8 +1,5 @@
 """
 Parser for Thunderboard BLE advertisements.
-
-This file is shamelessly copied from the following repository:
-https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/oral_b.py
 """
 from __future__ import annotations
 
